3207c41d14d0100b4388b0499fdd10446e84c7f1
pencilbox007 <90339306+pencilbox007@users.noreply.github.com>
2021-09-10 20:09:42 +0530

    Update AS.java

--- AS.java
+++ AS.java
@@ -1,19 +1,3 @@
-/*
-	Anurag Sort
-	
-	Unsorted : 15 6 13 22 3 52 2
-	
-	For ASC Order
-	find max=52
-	looping 
-		find min=2
-		store min in res[]
-		store the max+1 at min val index
-		
-		0 => ASC
-		1 => DESC
-*/
-
 import java.util.Scanner;
 import java.util.Arrays;
 class AS
